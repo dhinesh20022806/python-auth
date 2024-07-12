bbc659be58f98341df6c513bd909c4f93135ad00
dhinesh20022806 <dhinesh20022806@gmail.com>
2024-07-12 21:08:31 +0530

    remove .idea

--- controllers/register.py
+++ controllers/register.py
@@ -1,49 +1,39 @@
-from flask import render_template, request, make_response
+from flask import render_template, request
 from re import match
 
 from encrypt import hash_password
 from models.register import insert_table
-from models.login import select_table
-
 
 def get_register(email_error=None, password_error=None, confirm_password_error=None):
-    return render_template('register.html', email_error=email_error, password_error=password_error,
-                           confirm_password_error=confirm_password_error)
+	return render_template('register.html', email_error=email_error, password_error=password_error, confirm_password_error=confirm_password_error)
 
 
 def post_register():
-    email = request.form['email']
-    plain_text = request.form['password']
-    plain_text_confirm = request.form['confirm-password']
+	email = request.form['email']
+	plain_text = request.form['password']
+	plain_text_confirm = request.form['confirm-password']
 
-    regx_email = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+	regx_email = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
 
-    verify_email = match(regx_email, email)
+	verify_email = match(regx_email, email)
+	if plain_text != plain_text_confirm :
+		return register();
 
-    regx_plain_text = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
-    verify_plain_text = match(regx_plain_text, plain_text)
+	regx_plain_text = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
+	verify_plain_text = match(regx_plain_text, plain_text)
 
-    data = select_table(email)
+	if not verify_email :
+		return get_register('invalid email address')
 
-    if data:
-        return get_register('email already exist')
+	if len(plain_text) < 8:
+		return get_register( None, 'Minium length 8 characters')
 
-    if not verify_email:
-        return get_register('invalid email address')
+	if not verify_plain_text:
+    return get_register( None, '1 uppercase, 1 lowercase, 1 digit, 1 special character')
 
-    if len(plain_text) < 8:
-        return get_register(None, 'Minium length 8 characters')
+  if plain_text != plain_text_confirm:
+    return get_register(None,'password should be same', 'password should be same')
+	password = hash_password(plain_text)
+	insert_table(email, password)
+	return '<h1>you register successfully!</h1>'
 
-    if not verify_plain_text:
-        return get_register(None, '1 uppercase, 1 lowercase, 1 digit, 1 special character')
-
-    if plain_text != plain_text_confirm:
-        return get_register(None, 'password should be same', 'password shold be same')
-
-    password = hash_password(plain_text)
-    insert_table(email, password)
-
-    resp = make_response('<h1>you register successfully!</h1>')
-    resp.set_cookie('email', email)
-    resp.set_cookie('password',plain_text)
-    return resp
--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from flask import Flask
 
+
 from routers.home import home_blueprint
 from routers.login import login_blueprint
 from routers.register import register_blueprint
+
+
 
 app = Flask(__name__)
 
 app.register_blueprint(home_blueprint, url_prefix='/')
 
-app.register_blueprint(login_blueprint, url_prefix='/login')
+app.register_blueprint(login_blueprint,url_prefix='/login')
 
 app.register_blueprint(register_blueprint, url_prefix='/register')
 
+
 if __name__ == '__main__':
-    app.run(
-        host="127.0.0.1",
-        port="5000",
-        debug=True
-    )
+	app.run(
+		host="127.0.0.1",
+		port="5000",
+		debug=True
+		)
